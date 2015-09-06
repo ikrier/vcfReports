ba3fbb657bed2558d262e504fef9db1abdf17b26
Irina Krier <irina.krier@gmail.com>
2015-09-06 15:33:31 +0200

    first commit

---
+++ run_sweave_vcfname.R
@@ -0,0 +1,26 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+libname=args[1]
+diamtablename=args[2]
+vcffile=args[3]
+regionsname=args[4]
+ampliconsname=args[5]
+pindelresults=args[6]
+
+for(i in 1:5)
+{
+  cat(args[i],"\n")
+}
+
+system(command = paste("mkdir variantreportdir",libname,sep="_"))
+setwd(paste("variantreportdir",libname,sep="_"))
+
+diamtable=read.csv(diamtablename,header=F)
+
+diamname=diamtable$V2[as.numeric(libname)]
+if(diamname=="NONE"){diamname="Aucun"}
+
+Sweave("/data/vcfReports/VariantsReport.Rnw",)
+tools::texi2pdf("VariantsReport.tex")
+
+system(command=paste("mv VariantsReport.pdf ../VariantsReport_Lib",libname,".pdf",sep=""))
